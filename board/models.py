97d9d1a1b4df4b6b79c1d35b7526989ee42d95b4
Jeevan M R <14.jeevan@gmail.com>
2021-02-18 12:34:32 +0530

    Board: Get & List API

--- board/models.py
+++ board/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib import admin
 
-# Create your models here.
+
+class Board(models.Model):
+    name = models.CharField(max_length=255)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    members = models.ManyToManyField(User, related_name="members")
+    moderators = models.ManyToManyField(User, related_name="moderators")
+    banned = models.ManyToManyField(User, related_name="banned")
+
+    @property
+    def board_id(self):
+        return '-'.join([str(self.id), self.name])
+
+    def __str__(self):
+        return self.name
+
+
+admin.site.register(Board)
--- board/views.py
+++ board/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from rest_framework import serializers, viewsets
+from rest_framework import permissions
+from board.models import Board
 
-# Create your views here.
+
+class BoardSerializer(serializers.HyperlinkedModelSerializer):
+    is_member = serializers.SerializerMethodField()
+    is_moderator = serializers.SerializerMethodField()
+    is_banned = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Board
+        fields = ['name', 'is_member', 'is_moderator', 'is_banned']
+
+    def get_is_member(self, obj):
+        user = self.get_user()
+        if user:
+            return user in obj.members.all()
+        return False
+
+    def get_is_moderator(self, obj):
+        user = self.get_user()
+        if user:
+            return user in obj.moderators.all()
+        return False
+
+    def get_is_banned(self, obj):
+        user = self.get_user()
+        if user:
+            return user in obj.banned.all()
+        return False
+
+    def get_user(self):
+        user = None
+        request = self.context.get("request")
+        if request and hasattr(request, "user"):
+            user = request.user
+        return user
+
+
+class BoardViewSet(viewsets.ModelViewSet):
+    queryset = Board.objects.all()
+    serializer_class = BoardSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
